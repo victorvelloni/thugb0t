7dbd7156d0f48387eadf5d59b036471b368df2c5
victorvelloni <victor.velloni@gmail.com>
2021-01-29 02:37:18 -0300

    adicionada retorno de sucesso ao criar tuíte

--- tuites.py
+++ tuites.py
@@ -1,6 +1,11 @@
 import autenticador
 import random
+from flask import Flask
+from flask import Response
 
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/', methods = ['POST'])
 def gerar_tt():
     api = autenticador.autenticar()
 
@@ -15,6 +20,7 @@ def gerar_tt():
         verso += letra_inteira[numero_do_verso+1]
 
     api.update_status(verso)
+    return Response("{'status': 'success'}", status=200, mimetype='application/json')
 
-if __name__ == "__main__":
-    gerar_tt()
+if __name__ == '__main__':
+    app.run(debug=True)
