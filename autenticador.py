d1f72d0d15e227d9e9e32eb09252098ef60e5ccb
victorvelloni <victor.velloni@gmail.com>
2021-02-10 00:53:09 -0300

    add enviroment variables

--- autenticador.py
+++ autenticador.py
@@ -1,11 +1,12 @@
 import tweepy
+import os
 
 def autenticar():
-    consumer_key = "mDQiWnpgG2SBQJBqMMxBtQDmL"
-    consumer_secret = "BH0UB599jUCXxwmgC58c2jAUiNMq95aVbQsUDJxM9yEmCgbgCI"
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
 
-    access_token = "1326267633063309312-mbtW1yn7GuWdNly4IPuApG85UGmgGz"
-    access_token_secret = "glNOmPtvwjAxNUu7gIY7YP3HbT5h4qrMBtBLeFRdhCow5"
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
